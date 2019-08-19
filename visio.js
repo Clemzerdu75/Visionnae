e02921dad42a5998bf198465af9175435685ace7
Clement FAUVELLE <cfauvell@e1r5p4.42.fr>
2019-08-19 14:01:31 +0200

    More responsive design

--- visio.js
+++ visio.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
 
 	/*Good size for div*/
 		var wheight = $(window).height();
+		var wwidth = $(window).width();
 		$('.parallax').css("height", wheight);
 		$('.content-wrapper').css("height", wheight * 2);
 		$('.contact').css("height", wheight);
 		$('.footer').css("height", wheight / 3);
+		$('.layer3').css("margin-top", wheight / 3.5);
 
 	/*METHODES global animation*/
 		var scene2 = new ScrollMagic.Scene({
@@ -26,20 +28,19 @@ $(document).ready(function(){
 		.setClassToggle('.content', 'show')
 		.addTo(controller);
 
-	/*METHODES secondary animation*/
-		var scene = new ScrollMagic.Scene({
-			triggerElement: '.some-page-wrapper',
-			duration: 900,
-			offset: 0,
-		})
-		.setClassToggle('.some-page-wrapper', 'show')
-		.addTo(controller);
-
 	/*OFFRES global animation*/
-		var scene4 = new ScrollMagic.Scene({
-			triggerElement: '.content2',
-			offset: wheight / 2.3,
-		})
-		.setClassToggle('.content2', 'show')
-		.addTo(controller);
+	var scene2 = new ScrollMagic.Scene({
+		triggerElement: '.content2',
+		duration: 800,
+		offset: wheight / 2,
+	})
+	.setPin(".content2")
+	.addTo(controller);
+
+	var scene4 = new ScrollMagic.Scene({
+		triggerElement: '.content2',
+		offset: wheight / 2.3,
+	})
+	.setClassToggle('.content2', 'show')
+	.addTo(controller);
 })
